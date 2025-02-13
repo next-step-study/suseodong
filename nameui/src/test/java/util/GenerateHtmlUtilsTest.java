9f3ad0fb5c1091ae53a49b98b72c0eb5f672105d
5nam <ohnam90@gmail.com>
2025-02-13 20:34:48 +0900

    [test] : GenerateHtmlUtils 테스트 코드 추가

---
+++ nameui/src/test/java/util/GenerateHtmlUtilsTest.java
@@ -0,0 +1,30 @@
+package util;
+
+import junit.framework.TestCase;
+import model.User;
+import org.junit.Test;
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertThat;
+
+public class GenerateHtmlUtilsTest extends TestCase {
+
+    @Test
+    public void testGenerateUserTableHtml() {
+        //given
+        List<User> users = new ArrayList<>();
+        users.add(new User("userId1", "ddd", "user1", "ddd@naver.com"));
+        users.add(new User("userId2", "dddd", "user2", "dddd@naver.com"));
+
+        //when
+        String userTableHtml = GenerateHtmlUtils.generateUserTableHtml(users);
+
+        //then
+        assertThat(userTableHtml, containsString("user1"));
+        assertThat(userTableHtml, containsString("user2"));
+    }
+}
