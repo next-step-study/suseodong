720754ebccff28dae2440a4cd9fb5f1213b1aa2e
hyunn522 <tjgus52213@gmail.com>
2025-03-08 16:15:42 +0900

    feat: 개인정보수정 실습
    [6.1.2]

---
+++ seohyeon/ch6_12/src/main/java/next/web/UpdateUserServlet.java
@@ -0,0 +1,25 @@
+package next.web;
+
+import core.db.DataBase;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import next.model.User;
+
+@WebServlet("/users/update")
+public class UpdateUserServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String userId = req.getParameter("userId");
+        String password = req.getParameter("password");
+        String name = req.getParameter("name");
+        String email = req.getParameter("email");
+        DataBase.addUser(new User(userId, password, name, email));
+
+        resp.sendRedirect("/user/list");
+    }
+}
