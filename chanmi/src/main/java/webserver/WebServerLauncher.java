70ba2e62395bddc97d0b796074912f62f84c5506
05AM <anytime0224@gmail.com>
2025-03-22 15:23:41 +0900

    feat: Hello world servlet 클래스 구현 및 WebServerLauncher 분리
    - run할 때 configuration의 `cp(class path)` 옵션을 maven pom.xml에서 정의한 `web-application-server`로 설정하지 않으면 실행 실패
    - pom.xml 등의 설정은 intellij의 설정을 덮어씌운다. 일치하지 않는 경우가 있음

---
+++ chanmi/src/main/java/webserver/WebServerLauncher.java
@@ -0,0 +1,26 @@
+package webserver;
+
+import java.io.File;
+
+import org.apache.catalina.connector.Connector;
+import org.apache.catalina.startup.Tomcat;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class WebServerLauncher {
+    private static final Logger log = LoggerFactory.getLogger(WebServerLauncher.class);
+    private static final int DEFAULT_PORT = 8080;
+    private static final String WEBAPP_DIR_LOCATION = "webapp/";
+
+    public static void main(String args[]) throws Exception {
+        Tomcat tomcat = new Tomcat();
+        tomcat.setPort(DEFAULT_PORT);
+        Connector connector = tomcat.getConnector();
+        connector.setURIEncoding("UTF-8");
+        tomcat.addWebapp("/", new File(WEBAPP_DIR_LOCATION).getAbsolutePath());
+        log.info("Starting webapp from {}", new File(WEBAPP_DIR_LOCATION).getAbsolutePath());
+
+        tomcat.start();
+        tomcat.getServer().await();
+    }
+}
